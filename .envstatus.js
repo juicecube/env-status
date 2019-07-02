ddd73bd8b65abb73b6e74b3f2b4ea664477357f3
webyom <webyom@gmail.com>
2019-07-02 08:36:03 +0800

    add development-1 env

--- .envstatus.js
+++ .envstatus.js
@@ -1,5 +1,5 @@
 module.exports = {
-  envs: ['production', 'staging', 'development'],
+  envs: ['production', 'staging', 'development', 'development-1'],
   url: function (env) {
     return `https://raw.githubusercontent.com/webyom/env-status/master/envs/${env}.json`;
   }
