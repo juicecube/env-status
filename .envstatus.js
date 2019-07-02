34c34ca0920da19f18aae7b8f99c15f0d2d4d57e
webyom <webyom@gmail.com>
2019-07-02 09:58:53 +0800

    rename env

--- .envstatus.js
+++ .envstatus.js
@@ -1,5 +1,5 @@
 module.exports = {
-  envs: ['production', 'staging', 'development', 'development-1', 'development-2'],
+  envs: ['production', 'staging', 'dev', 'dev1', 'dev2', 'dev3'],
   url: function (env) {
     return `https://raw.githubusercontent.com/webyom/env-status/master/envs/${env}.json`;
   }
