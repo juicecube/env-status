8fe9e90d169c87c7e5670ac0a0222eaece61e64b
webyom <webyom@gmail.com>
2020-09-23 12:36:38 +0800

    fix: env-build-validate shell script

--- env-build-validate.sh
+++ env-build-validate.sh
@@ -9,7 +9,7 @@ fi
 
 echo "Build $branch branch in $env environment."
 
-if [[ $branch != "master" && ! $branch =~ sprint/. && ! $branch =~ feat/. && ! $branch =~ fix/. && ! $branch =~ hotfix/. ]]
+if [[ $branch != "master" && ! $branch =~ ^sprint/. && ! $branch =~ ^feat/. && ! $branch =~ ^fix/. && ! $branch =~ ^hotfix/. ]]
 then
   echo "!!! invalid branch name !!!"
   exit 1
@@ -17,7 +17,7 @@ fi
 
 if [[ $env = "production" || $env = "staging" || $env = "test" ]]
 then
-  if [[ $branch != "master" && ! $branch =~ sprint/. && ! $branch =~ hotfix/. ]]
+  if [[ $branch != "master" && ! $branch =~ ^sprint/. && ! $branch =~ ^hotfix/. ]]
   then
     echo "!!! only master, sprint and hotfix branches can be built in production, staging and test environment !!!"
     exit 2
